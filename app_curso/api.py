27cd4f1ed92057c7a09faeac2bebc4f6cc2c5c35
FabioPortella <fabioportella2009@gmail.com>
2024-07-06 01:10:53 -0300

    criando POST e PUT em Tipos de Curso

--- app_curso/api.py
+++ app_curso/api.py
@@ -6,7 +6,6 @@ from .models import Curso, TipoCurso
 
 api = NinjaAPI()
 
-
 @api.get('/tipocurso', response=List[TipoCursoSchema])
 def listar(request):
     return TipoCurso.objects.all()
@@ -19,3 +18,44 @@ def obter(request, id: int):
         return tipocurso
     except TipoCurso.DoesNotExist as e:
         return 404, {"message": "Tipo de Curso não cadastrado"}
+    
+@api.post('/tipocurso', response={201: TipoCursoSchema, 400: NotFoundSchema})
+def inserir(request, tipoCurso: TipoCursoSchema) -> TipoCurso:
+    try:
+        # Verifica se o nome tem menos de três caracteres ou se é composto apenas por espaços em branco
+        if len(tipoCurso.nome.strip()) < 3:
+            return 400, {"message": "O nome do Tipo de Curso deve ter pelo menos três caracteres e não pode ser composto por espaços em branco."}
+        
+        # Verifica se já existe um TipoCurso com o mesmo nome
+        if TipoCurso.objects.filter(nome=tipoCurso.nome).exists():
+            return 400, {"message": "Tipo de Curso com este nome já existe."}
+        
+        # Cria um novo TipoCurso
+        tipoCurso = TipoCurso.objects.create(**tipoCurso.dict())
+        return 201, tipoCurso
+    except Exception as e:
+        return 400, {"message": "Erro ao salvar um novo Tipo de Curso"}
+    
+
+@api.put('/tipocurso/{id}', response={200: TipoCursoSchema, 404: NotFoundSchema, 400: NotFoundSchema})
+def atualizar(request, id: int, tipoCurso: TipoCursoSchema) -> TipoCurso:
+    try:
+        # Verifica se o TipoCurso existe
+        try:
+            tipo_curso_atual = TipoCurso.objects.get(pk=id)
+        except TipoCurso.DoesNotExist:
+            return 404, {"message": "Tipo de Curso não encontrado."}
+        
+        # Verifica se o nome tem menos de três caracteres ou se é composto apenas por espaços em branco
+        if len(tipoCurso.nome.strip()) < 3:
+            return 400, {"message": "O nome do Tipo de Curso deve ter pelo menos três caracteres e não pode ser composto por espaços em branco."}
+        
+        # Atualiza os dados do TipoCurso
+        tipo_curso_atual.nome = tipoCurso.nome
+        tipo_curso_atual.descricao = tipoCurso.descricao
+        tipo_curso_atual.save()
+        
+        return 200, tipo_curso_atual
+    except Exception as e:
+        return 400, {"message": "Erro ao atualizar o Tipo de Curso"}
+
